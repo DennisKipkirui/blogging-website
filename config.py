4c1f8a7d61d93a4f356e9ef53d389299176b0fc3
DennisKipkirui <kipkiruimutai33@gmail.com>
2020-11-03 23:38:58 +0300

    add upload images and edit file

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog12'
+    
+    SECRET_KEY = 'Qwerty'
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog12'
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+    Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
+
+    
